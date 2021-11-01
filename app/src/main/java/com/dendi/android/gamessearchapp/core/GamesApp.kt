d405227405b2dd7965d7dff42e1ec3efd33a8f66
Dendi188 <76904012+Dendi188@users.noreply.github.com>
2021-11-01 15:59:43 +0200

    update manifest and gradle dependencies

---
+++ app/src/main/java/com/dendi/android/gamessearchapp/core/GamesApp.kt
@@ -0,0 +1,10 @@
+package com.dendi.android.gamessearchapp.core
+
+import android.app.Application
+
+/**
+ * @author Dendy-Jr on 01.11.2021
+ * olehvynnytskyi@gmail.com
+ */
+class GamesApp: Application() {
+}
