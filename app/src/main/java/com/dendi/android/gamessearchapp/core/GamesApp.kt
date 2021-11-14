2187b4148375e9b3ca1ff7aca0d30b5988d11f25
Dendi188 <76904012+Dendi188@users.noreply.github.com>
2021-11-14 10:15:53 +0200

    add Service Locator

--- app/src/main/java/com/dendi/android/gamessearchapp/core/GamesApp.kt
+++ app/src/main/java/com/dendi/android/gamessearchapp/core/GamesApp.kt
@@ -1,35 +1,14 @@
 package com.dendi.android.gamessearchapp.core
 
 import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.viewbinding.BuildConfig
-import com.dendi.android.gamessearchapp.data.games.BaseGamesRepository
-import com.dendi.android.gamessearchapp.data.core.GamesDatabase
-import com.dendi.android.gamessearchapp.data.detail.DetailRepositoryImpl
-import com.dendi.android.gamessearchapp.data.detail.DbObjectMapperBase
-import com.dendi.android.gamessearchapp.data.detail.DetailDataMapperBase
-import com.dendi.android.gamessearchapp.data.detail.cache.DetailCacheDataSource
-import com.dendi.android.gamessearchapp.data.detail.cloud.DetailCloudDataSource
-import com.dendi.android.gamessearchapp.data.detail.cloud.DetailService
-import com.dendi.android.gamessearchapp.data.games.DataGameMapper
-import com.dendi.android.gamessearchapp.data.games.DbGameMapper
-import com.dendi.android.gamessearchapp.data.games.cache.GamesCacheDataSource
-import com.dendi.android.gamessearchapp.data.games.cloud.GameService
-import com.dendi.android.gamessearchapp.data.games.cloud.GamesCloudDataSource
-import com.dendi.android.gamessearchapp.domain.detail.DetailDomainMapperBase
-import com.dendi.android.gamessearchapp.domain.detail.DetailInteractor
-import com.dendi.android.gamessearchapp.domain.detail.HandlerDomainMapperBase
-import com.dendi.android.gamessearchapp.domain.games.*
-import com.dendi.android.gamessearchapp.presentation.detail.DetailCommunication
-import com.dendi.android.gamessearchapp.presentation.detail.DetailUiMapperBase
-import com.dendi.android.gamessearchapp.presentation.detail.DetailViewModel
-import com.dendi.android.gamessearchapp.presentation.detail.HandlerUiMapperBase
-import com.dendi.android.gamessearchapp.presentation.games.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.dendi.android.gamessearchapp.sl.core.CoreModule
+import com.dendi.android.gamessearchapp.sl.core.DependencyContainer
+import com.dendi.android.gamessearchapp.sl.core.ViewModelFactory
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 /**
  * @author Dendy-Jr on 01.11.2021
@@ -37,81 +16,20 @@ import java.util.concurrent.TimeUnit
  */
 class GamesApp : Application() {
 
-    lateinit var gameViewModel: GamesViewModel
-    private lateinit var gamesInteractor: GamesInteractor
+    private val coreModule = CoreModule()
 
-    lateinit var detailViewModel: DetailViewModel
-    private lateinit var detailInteractor: DetailInteractor
+    private val factory by lazy {
+        ViewModelFactory(DependencyContainer.Base(coreModule))
+    }
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-
-        val client = OkHttpClient.Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://www.freetogame.com/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-
-        val gameService = retrofit.create(GameService::class.java)
-        val detailService = retrofit.create(DetailService::class.java)
-
-        val gameDao = GamesDatabase.database(this).gameDao()
-        val detailDao = GamesDatabase.database(this).detailDao()
-
-        val resourceProvider = ResourceProvider.Base(this)
-
-        val gameDataMapper = DataGameMapper()
-        val detailDataMapper = DetailDataMapperBase()
-        val detailDomainMapperBase = DetailDomainMapperBase()
-        val handlerDomainBase = HandlerDomainMapperBase(detailDomainMapperBase)
-
-        val dbGameMapper = DbGameMapper()
-        val domainGameMapper = DomainGameMapper()
-        val domainGamesMapper = BaseGamesDataToDomainMapper(domainGameMapper)
-        val uiGameMapper = UiGameMapper()
-        val uiGamesMapper =
-            BaseGamesDomainToUiMapper(resourceProvider,uiGameMapper)
-
-        val detailDbMapper = DbObjectMapperBase()
-
-        val gamesCloudDataSource = GamesCloudDataSource.Base(gameService)
-        val gamesCacheDataSource = GamesCacheDataSource.Base(gameDao, dbGameMapper)
-        val detailCloudDataSource = DetailCloudDataSource.Base(detailService)
-        val detailCacheDataSource = DetailCacheDataSource.Base(detailDao, detailDbMapper)
-        val gamesRepository = BaseGamesRepository(
-            gamesCloudDataSource,
-            gamesCacheDataSource,
-            gameDataMapper
-        )
-        val detailUiMapper = DetailUiMapperBase()
-        val handlerUiMapper = HandlerUiMapperBase(detailUiMapper, resourceProvider)
-
-        val detailRepository =
-            DetailRepositoryImpl(detailCloudDataSource, detailCacheDataSource, detailDataMapper)
-
-        gamesInteractor = GamesInteractor.Base(gamesRepository,
-            domainGamesMapper, domainGameMapper)
-
-        detailInteractor = DetailInteractor.Base(detailRepository, handlerDomainBase)
-
-        val gamesCommunication = GamesCommunication.Base()
-        val detailCommunication = DetailCommunication.Base()
-
-        gameViewModel = GamesViewModel(
-            gamesInteractor, uiGamesMapper, uiGameMapper, gamesCommunication
-        )
-
-        detailViewModel = DetailViewModel(detailInteractor, detailCommunication, handlerUiMapper)
+        coreModule.init(this)
     }
+
+    fun <T : ViewModel> viewModel(modelClass: Class<T>, owner: ViewModelStoreOwner): T =
+        ViewModelProvider(owner, factory).get(modelClass)
 }
---
+++ app/src/main/java/com/dendi/android/gamessearchapp/sl/core/CoreModule.kt
@@ -0,0 +1,56 @@
+package com.dendi.android.gamessearchapp.sl.core
+
+import android.content.Context
+import com.dendi.android.gamessearchapp.core.ResourceProvider
+import com.dendi.android.gamessearchapp.core.ScrollPositionCache
+import com.dendi.android.gamessearchapp.data.core.GamesDatabase
+import com.dendi.android.gamessearchapp.data.detail.cache.DetailDao
+import com.dendi.android.gamessearchapp.data.games.cache.GameDao
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author Dendy-Jr on 12.11.2021
+ * olehvynnytskyi@gmail.com
+ */
+class CoreModule {
+
+    private lateinit var retrofit: Retrofit
+    lateinit var resourceProvider: ResourceProvider
+    lateinit var client: OkHttpClient
+    lateinit var gameDao: GameDao
+    lateinit var detailDao: DetailDao
+    lateinit var scrollPositionCache: ScrollPositionCache
+
+    fun init(context: Context) {
+
+        client = OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        resourceProvider = ResourceProvider.Base(context)
+        gameDao = GamesDatabase.database(context).gameDao()
+        detailDao = GamesDatabase.database(context).detailDao()
+
+        scrollPositionCache = ScrollPositionCache.Base(resourceProvider)
+    }
+
+    fun <T> networkService(clazz: Class<T>) = retrofit.create(clazz)
+
+    private companion object {
+        const val BASE_URL = "https://www.freetogame.com/api/"
+    }
+
+}
